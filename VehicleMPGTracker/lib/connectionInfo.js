cde5f7f1cdb2ef0d4c1a4a0a07806ad46551928d
RobertNye22 <97561854+RobertNye22@users.noreply.github.com>
2023-04-28 13:05:40 -0600

    created sql tables

---
+++ VehicleMPGTracker/lib/connectionInfo.js
@@ -0,0 +1,9 @@
+
+let connectionInfo = {
+    host: "127.0.0.1",
+    port: 3306,
+    user: "RobertNye1",
+    password: "@mr.yeastonrm850"
+}
+
+module.exports = connectionInfo;
--- VehicleMPGTracker/lib/database.js
+++ VehicleMPGTracker/lib/database.js
@@ -1,11 +1,15 @@
 let mysql = require('mysql2');
 
-let con = mysql.createConnection({
-    host: "127.0.0.1",
-    port: 3306,
-    user: "RobertNye1",
-    password: "@mr.yeastonrm850"
-});
+var dbConnectionInfo = require('./connectionInfo');
+
+var con = mysql.createConnection({
+    host: dbConnectionInfo.host,
+    user: dbConnectionInfo.user,
+    password: dbConnectionInfo.password,
+    port: dbConnectionInfo.port,
+    multipleStatements: true              // Needed for stored proecures with OUT results
+  });
+
 
 con.connect(function(err) {
     if (err) throw err;
@@ -21,10 +25,91 @@ function selectDatabase() {
     con.query("USE VehicleMPGTracker", function (err, result) {
         if (err) throw err;
         console.log("Database selected");
-        createTable();
+        createTables();
     });
 }
 
-function createTable() {}
+function createTables() {
+    // A CREATE TABLE call will work if it does not exist or if it does exist.
+    // Either way, that's what we want.
+    let sql =   "CREATE TABLE IF NOT EXISTS user (\n" +
+                "UserID INT NOT NULL AUTO_INCREMENT,\n" +
+                "UserFirstName VARCHAR(25) NOT NULL,\n" +
+                "UserLastName VARCHAR(25) NOT NULL,\n" +
+                "UserName VARCHAR(35) NOT NULL,\n" +
+                "UserHashedPassword VARCHAR(255) NOT NULL,\n" +
+                "UserSalt VARCHAR(255) NOT NULL,\n" +
+                "PRIMARY KEY (UserID)\n" +
+              ")";
+    con.execute(sql, function(err, results, fields) {
+      if (err) {
+        console.log(err.message);
+        throw err;
+      } else {
+        console.log("database.js: table User created if it didn't exist");
+      }
+    });
+
+    sql =       "CREATE TABLE IF NOT EXISTS vehicle (\n" +
+                "VehicleID INT NOT NULL AUTO_INCREMENT,\n" +
+                "VehicleYear INT NOT NULL,\n" +
+                "VehicleMake VARCHAR(25) NOT NULL,\n" +
+                "VehicleModel VARCHAR(25) NOT NULL,\n" +
+                "VehicleMilage DECIMAL NOT NULL,\n" +
+                "VehicleMPG DECIMAL NULL,\n" +
+                "VehicleTankSize DECIMAL NOT NULL,\n" +
+                "UserID INT NOT NULL,\n" +
+                "PRIMARY KEY (VehicleID),\n" +
+                "FOREIGN KEY (UserID) REFERENCES user(UserID)\n" +
+              ")";
+    con.execute(sql, function(err, results, fields) {
+      if (err) {
+        console.log(err.message);
+        throw err;
+      } else {
+        console.log("database.js: table Vehicle created if it didn't exist");
+      }
+    });
+
+    sql =       "CREATE TABLE IF NOT EXISTS upgrade (\n" +
+                "UpgradeID INT NOT NULL AUTO_INCREMENT,\n" +
+                "UpgradeDate DATE NOT NULL,\n" +
+                "UpgradePartType VARCHAR(25) NOT NULL,\n" +
+                "UpgradePartName VARCHAR(25) NOT NULL,\n" +
+                "UpgradeCurrentMilage DECIMAL NOT NULL,\n" +
+                "VehicleID INT NOT NULL,\n" +
+                "PRIMARY KEY (UpgradeID),\n" +
+                "FOREIGN KEY (VehicleID) REFERENCES vehicle(VehicleID)\n" +
+              ")";
+    con.execute(sql, function(err, results, fields) {
+      if (err) {
+        console.log(err.message);
+        throw err;
+      } else {
+        console.log("database.js: table Upgrade created if it didn't exist");
+      }
+    });
+
+    sql =       "CREATE TABLE IF NOT EXISTS fillup (\n" +
+                "FillUpID INT NOT NULL AUTO_INCREMENT,\n" +
+                "FillUpDate DATE NOT NULL,\n" +
+                "FillUpGallons DECIMAL NOT NULL,\n" +
+                "FillUpCurrentMilage DECIMAL NOT NULL,\n" +
+                "VehicleID INT NOT NULL,\n" +
+                "PRIMARY KEY (FillUpID),\n" +
+                "FOREIGN KEY (VehicleID) REFERENCES vehicle(VehicleID)\n" +
+              ")";
+    con.execute(sql, function(err, results, fields) {
+      if (err) {
+        console.log(err.message);
+        throw err;
+      } else {
+        console.log("database.js: table FillUp created if it didn't exist");
+      }
+    });
+
+
+
+}
 
 module.exports = con;
